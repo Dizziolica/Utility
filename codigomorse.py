b32e668a8f39075638be04972e2aa3e081446f23
Caroline Oliveira <104214541+Dizziolica@users.noreply.github.com>
2022-11-13 19:31:31 +0000

    Update codigomorse.py

--- codigomorse.py
+++ codigomorse.py
@@ -1,42 +1,43 @@
-codigomorse = { "A":".-" ,
-"B": "-...", 
-"C": "-.-.", 
-"D": "-..", 
-"E": ".", 
-"F": "..-.", 
-"G": "--.", 
-"H": "....",
-"I": "..", 
-"J": ".---", 
-"K": "-.-", 
-"L": ".-..", 
-"M": "--", 
-"N": "-.", 
-"O": "---", 
-"P": ".--.", 
-"Q": "--.-", 
-"R": ".-.",
-"S": "...", 
-"T": "- ", 
-"U": "..-", 
-"V": "...-",
-" W": ".--", 
-"X": "-..-", 
-"Y": "-.--",
- "Z": "--.."}
+def codigomorse(mensagem):
+    codigomorse = { "A":".-" ,
+    "B": "-...", 
+    "C": "-.-.", 
+    "D": "-..", 
+    "E": ".", 
+    "F": "..-.", 
+    "G": "--.", 
+    "H": "....",
+    "I": "..", 
+    "J": ".---", 
+    "K": "-.-", 
+    "L": ".-..", 
+    "M": "--", 
+    "N": "-.", 
+    "O": "---", 
+    "P": ".--.", 
+    "Q": "--.-", 
+    "R": ".-.",
+    "S": "...", 
+    "T": "- ", 
+    "U": "..-", 
+    "V": "...-",
+    "W": ".--", 
+    "X": "-..-", 
+    "Y": "-.--",
+    "Z": "--.."}
 
-palavra = input("Texto para converter em codigo morse:  ").upper()
-lista= list(palavra)
-comprimento = len(lista)
+    palavra = input("Texto para converter em codigo morse:  ").upper()
+    lista= list(palavra)
+    comprimento = len(lista)
 
 
-for i in lista:
+    for i in lista:
     
-    if i != "":
-        try:
-            print(i)
-            print(codigomorse[i])
+        if i != "":
+            try:
+                print(i)
+                print(codigomorse[i])
         
-        except:
-            print("")
+            except:
+                print("Error, Try Again")
 
